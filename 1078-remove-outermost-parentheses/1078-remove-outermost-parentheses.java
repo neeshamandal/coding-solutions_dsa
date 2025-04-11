7f9fc9a3f4369d4c3dda5ca5d0c7bd22aa274713
neeshamandal <45892661+neeshamandal@users.noreply.github.com>
2025-04-12 01:23:02 +0530

    Time: 4 ms (62.17%), Space: 42.5 MB (32.22%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -3,21 +3,19 @@ class Solution {
         StringBuilder result = new StringBuilder();
         int balance = 0;
         int start = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)=='('){
                 balance++;
-            } else {
+            }else{
                 balance--;
             }
-            
-            if (balance == 0) {
-                // Add the substring excluding the first and last characters
-                result.append(s.substring(start + 1, i));
-                start = i + 1;
+
+            if(balance==0){
+                result.append(s.substring(start+1,i));
+                start = i+1;
             }
         }
-        
+
         return result.toString();
     }
 }
